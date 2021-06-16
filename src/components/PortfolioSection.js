d2e17f22f81550c0a09dff291136811005043e7f
Stackbit Code Editor <projects@stackbit.com>
2021-06-16 22:36:09 +0000

    PortfolioSection.js: changed

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -41,7 +41,7 @@ export default class PortfolioSection extends React.Component {
         const team = _.get(section, 'portfolio');
 
         return (
-            <section className="section section--team">
+            <section className="section section--portfolio">
                 {title && (
                     <div className="container container--md align-center">
                         <h2 className="section__title">{title}</h2>
